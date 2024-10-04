798c40d616804db81ec1ca3dd2185e84ebd3e90a
TinyAbra <tiny.abra26@gmail.com>
2024-10-04 18:17:48 -0400

    fix links

--- quartz/components/custom/LinksHeader/LinksHeader.tsx
+++ quartz/components/custom/LinksHeader/LinksHeader.tsx
@@ -10,13 +10,13 @@ export default (() => {
     return (
       <div id="links-header-container">
         <div id="links-header">        
-            <a class="links-header-item" href="/Getting-Started/1.-Welcome">
+            <a class="links-header-item" href="/ttrpg-website/Getting-Started/1.-Welcome">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-book-open-check"><path d="M8 3H2v15h7c1.7 0 3 1.3 3 3V7c0-2.2-1.8-4-4-4Z"/><path d="m16 12 2 2 4-4"/><path d="M22 6V3h-6c-2.2 0-4 1.8-4 4v14c0-1.7 1.3-3 3-3h7v-2.3"/></svg>
             Getting Started</a>    
             <a class="links-header-item" href="https://github.com/TinyAbra/ttrpg-website">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-wrench"><path d="M14.7 6.3a1 1 0 0 0 0 1.4l1.6 1.6a1 1 0 0 0 1.4 0l3.77-3.77a6 6 0 0 1-7.94 7.94l-6.91 6.91a2.12 2.12 0 0 1-3-3l6.91-6.91a6 6 0 0 1 7.94-7.94l-3.76 3.76z"/></svg>
             Github</a>
-            <a class="links-header-item" href="/Getting-Started/Tips--and--Tricks/">
+            <a class="links-header-item" href="/ttrpg-website/Getting-Started/Tips--and--Tricks/">
               <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-file-json"><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z"/><path d="M14 2v4a2 2 0 0 0 2 2h4"/><path d="M10 12a1 1 0 0 0-1 1v1a1 1 0 0 1-1 1 1 1 0 0 1 1 1v1a1 1 0 0 0 1 1"/><path d="M14 18a1 1 0 0 0 1-1v-1a1 1 0 0 1 1-1 1 1 0 0 1-1-1v-1a1 1 0 0 0-1-1"/></svg>
             Programmers Reference</a>
         </div>
